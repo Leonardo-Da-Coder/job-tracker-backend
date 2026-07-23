d809c3e4e4b0bd24d7c0c29b88b513f7b9c245c1
L.T. <48217460+Leonardo-Da-Coder@users.noreply.github.com>
2026-07-23 14:26:53 +0200

    setup fastapi (#1)
    * setup get applications and post application
    
    * Add CRUD endpoints for applications and 404 error handling

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI , status, HTTPException
+from pydantic import BaseModel
+
+class Application(BaseModel):
+    firma: str
+    position: str
+    status: str
+    datum: str
+
+applications = [
+    Application(firma="DSV", position="Fullstack Entwickler", status="beworben", datum="20.07.2026"),
+    Application(firma="hiqs", position="Junior Go Developer", status="beworben", datum="20.07.2026"),
+    Application(firma="BAUHAUS", position="Junior Softwareentwickler", status="beworben", datum="20.07.2026"),
+]
+
+app = FastAPI()
+
+@app.get("/applications")
+def read_applications():
+    return applications
+
+@app.post("/applications", status_code=status.HTTP_201_CREATED)
+def add_application(application: Application):
+    applications.append(application)
+    return application
+
+@app.delete("/applications/{firma}", status_code=status.HTTP_200_OK)
+def delete_application(firma:str):
+    application = find_application(firma)
+    applications.remove(application)
+
+@app.patch("/applications/{firma}")
+def update_status(firma:str , status:str):
+    application = find_application(firma)
+    application.status = status
+
+def find_application(firma:str):
+    application = next((a for a in applications if a.firma == firma ), None)
+    if not application:
+        raise HTTPException(status_code=404, detail="Application not Found")
+    return application
